3d218cf9590259f0a6583faf6eb6f52fc21e3b4d
荠麦 <56749027+HeKvn@users.noreply.github.com>
2023-05-31 08:21:48 +0000

    feat: 处理body请求

---
+++ src/helpers/data.ts
@@ -0,0 +1,6 @@
+import { isPlainObject } from './util'
+
+export const transformRequest = (data: any): any => {
+  if (isPlainObject(data)) return JSON.stringify(data)
+  return data
+}
--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -1,9 +1,7 @@
 const toString = Object.prototype.toString
 
-export const isDate = (val: any): val is Date => {
-  return toString.call(val) === '[object Date]'
-}
+export const isDate = (val: any): val is Date => toString.call(val) === '[object Date]'
 
-export const isObject = (val: any): val is Object => {
-  return val !== null && typeof val === 'object'
-}
+export const isObject = (val: any): val is Object => val !== null && typeof val === 'object'
+
+export const isPlainObject = (val: any): val is Object => toString.call(val) === '[object, Object]'
